0d0d15ecbfcd7cd1385cafda2dc932c75adf235e
Ashan Madushanka <ashan.m@eyepax.com>
2019-02-23 15:15:31 +0530

    angular-13

--- Angular/src/app/common-component/header/header.component.ts
+++ Angular/src/app/common-component/header/header.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isLogginProgress = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginDevRant(){
+    this.isLogginProgress = true;
   }
 
 }
