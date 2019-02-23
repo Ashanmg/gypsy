c14a0d77cb3b88a5405da75fc05f8befae781ae2
Ashan Madushanka <ashan.m@eyepax.com>
2019-02-23 13:07:20 +0530

    angualar-6

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+// rant component
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./common-component/header/header.component";
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
