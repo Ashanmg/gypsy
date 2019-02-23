0635531e7d67bd7d0df2bfbdd315ac31354e00d0
Ashan Madushanka <ashan.m@eyepax.com>
2019-02-23 13:59:11 +0530

    angular-7

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 // rant component
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./common-component/header/header.component";
+import { LoaderComponent } from "./common-component/loader/loader.component";
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, LoaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
