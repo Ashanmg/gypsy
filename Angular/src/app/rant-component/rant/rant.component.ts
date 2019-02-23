c6b046ed20a81a826796f7ffc1c818193d8e549e
Ashan Madushanka <ashan.m@eyepax.com>
2019-02-24 01:59:10 +0530

    angular-37

--- Angular/src/app/rant-component/rant/rant.component.ts
+++ Angular/src/app/rant-component/rant/rant.component.ts
@@ -1,6 +1,8 @@
+import { Router, Routes } from '@angular/router';
 import { RantService } from './../../rant-service/rant.service';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { RantModel } from './rant.model';
+import { StorageService } from '../../rant-service/local-storage.service';
 
 @Component({
   selector: 'app-rant',
@@ -13,9 +15,13 @@ export class RantComponent implements OnInit {
   @Output() gettingrantList = new EventEmitter();
 
 
-  constructor(private rantService: RantService) { }
+  constructor(private rantService: RantService, private storageService: StorageService, private router: Router) { }
 
   ngOnInit() {
+    this.getRantList();
+  }
+
+  getRantList(){
     this.rantService.getRantList().subscribe(
       (obj : any) => {
         if(obj.ok){
@@ -30,5 +36,22 @@ export class RantComponent implements OnInit {
 
       }
     );
+  }
+
+  onVoteForRant(rant, isupvote){
+    if(this.storageService.getUserDetails() !== null){
+      const direction = isupvote ? "up" : "down";
+      this.rantService.setRantVote(rant.id, direction).subscribe((obj : any) => {
+        if(obj.ok){
+          this.getRantList();
+        }else{
+          console.log(obj);
+        }
+      }, error =>{
+
+      });
+    }else{
+
+    }
   }
 }
--- Angular/src/app/rant-service/rant.service.ts
+++ Angular/src/app/rant-service/rant.service.ts
@@ -11,4 +11,15 @@ export class RantService {
 
     return this.http.get(apiGetRantUrl);
   }
+
+  setRantVote(id, direction){
+    const params = {
+      postId: id,
+      direction: direction 
+    }
+
+    const apiVoteRantUrl = "https://api.devrant.thusitha.site/v1/post.vote";
+
+    return this.http.post(apiVoteRantUrl, params);
+  }
 }
