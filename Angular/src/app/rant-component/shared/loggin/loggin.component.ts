7e2fd8fadaba221ba00d1e6f1ed8e54759ccb11e
Ashan Madushanka <ashan.m@eyepax.com>
2019-02-23 15:01:16 +0530

    angular-11

--- Angular/src/app/rant-component/shared/loggin/loggin.component.ts
+++ Angular/src/app/rant-component/shared/loggin/loggin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loggin',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loggin.component.css']
 })
 export class LogginComponent implements OnInit {
+
+  @Input() isLogginPopupOpen = false;
 
   constructor() { }
 
